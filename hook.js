b9d8e6ebbe289fcec9b2155c3989445c23cf1a8b
melih <melihbirim@gmail.com>
2020-12-28 22:15:08 +0000

    updates in parsing body

--- hook.js
+++ hook.js
@@ -1,12 +1,45 @@
-let express = require("express");
-let app = express();
-let port = 6666
+const crypto = require('crypto');
+const express = require("express");
+const app = express();
+const port = 6666;
 
-app.post("/github", function (req, res) {
-	console.log('web hook received');
-	console.log(`Body: ${req.body}`);
-	let sender = req.body.sender;
-    	let branch = req.body.ref;
+const secret = 'SECRET_CHANGE_ME';
+
+const sigHeaderName = 'X-Hub-Signature'
+
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+
+app.use(express.json())
+
+function verify(req, res, next) {
+  	const payload = req.body.payload;
+  	if (!payload) {
+    		return next('Request body empty')
+  	}
+	//console.log(`paylod ${payload}`);
+	req.payload = JSON.parse(payload);
+  	const sig = req.get(sigHeaderName) || ''
+  	const hmac = crypto.createHmac('sha1', secret)
+  	const digest = Buffer.from('sha1=' + hmac.update(payload).digest('hex'), 'utf8')
+  	const checksum = Buffer.from(sig, 'utf8')
+  	if (checksum.length !== digest.length || !crypto.timingSafeEqual(digest, checksum)) {
+    		//return next(`Request body digest (${digest}) did not match ${sigHeaderName} (${checksum})`)
+  	}
+  	return next();
+}
+
+app.post("/", verify, function (req, res) {
+	if(req.body.payload == undefined || req.body == null){
+		return res.send(400);
+	}
+	const payload = req.payload;
+	console.log(`payload : ${payload}`);
+	let sender = req.payload.sender;
+    	let branch = req.payload.ref;
 	console.log(`Sender ${sender} at branch ${branch}`);
 	if(branch.indexOf('master') > -1){
 		deploy(res);
