fc837382a45fca4b5f98807bd6e3f5e865665665
Melih Birim <melih@tubu.io>
2021-01-11 01:25:43 +0300

    echo and deploy added

--- hook.js
+++ hook.js
@@ -3,26 +3,45 @@ let app = express();
 let port = 6666
 
 app.post("/github", function (req, res) {
-	console.log('web hook received');
+	console.log('github web hook received');
 	console.log(`Body: ${req.body}`);
+	if (!req.body) {
+		return res.send(500);
+	}
+	if (!req.body.sender) {
+		console.log('A non-github hook received, run echo.sh');
+		return echo(res);
+	}
 	let sender = req.body.sender;
-    	let branch = req.body.ref;
+    let branch = req.body.ref;
 	console.log(`Sender ${sender} at branch ${branch}`);
 	if(branch.indexOf('master') > -1){
-		deploy(res);
+		return deploy(res);
+	} else {
+		console.log('push not received from master branch');
+		return res.send(500);
 	} 
 })
 
 app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`)
+	console.log(`GitHub webhook listener started at http://localhost:${port}`)
 })
 
 function deploy(res){
-	childProcess.exec('cd /home/ubuntu && ./update.sh', function(err, stdout, stderr){
+	childProcess.exec('sh deploy.sh', function(err, stdout, stderr){
         	if (err) {
         		console.error(err);
         		return res.send(500);
         	}
         	res.send(200);
       	});
+}
+function echo(res) {
+	childProcess.exec('sh echo.sh', function (err, stdout, stderr) {
+		if (err) {
+			console.error(err);
+			return res.send(500);
+		}
+		res.send(200, stdout);
+	});
 }
